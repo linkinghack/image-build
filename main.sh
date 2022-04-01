ba047937ab375c9ce01b844e7fb09269a63286c1
linkinghack <linkinghack@outlook.com>
2022-04-01 16:56:07 +0800

    add cloud-ide-java build scripts

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
 # docker push linkinghack/eos-cdt-1.8.1-base:ubuntu18.04;
 
 # build cloud ide images
-docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/go-code-server:1.17 -f ./dockerfiles/cloud-ide/go.Dockerfile ./dockerfiles/cloud-ide --push;
-
+docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/go-code-server:go1.18-code4.2.0 -f ./dockerfiles/cloud-ide/go.Dockerfile ./dockerfiles/cloud-ide --push;
+docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/java-code-server:java8-11-18-code4.2.0 -f ./dockerfiles/cloud-ide/java.Dockerfile ./dockerfiles/cloud-ide --push;
