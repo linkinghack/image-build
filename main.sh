a5c93bfd4b48e87005a23fb87baa8a885e1fddc7
linkinghack <linkinghack@outlook.com>
2022-04-19 00:00:46 +0800

    build dbg-tool

--- main.sh
+++ main.sh
@@ -5,5 +5,9 @@ docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
 # docker push linkinghack/eos-cdt-1.8.1-base:ubuntu18.04;
 
 # build cloud ide images
-docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/go-code-server:go1.18-code4.3.0-v1 -f ./dockerfiles/cloud-ide/go.Dockerfile ./dockerfiles/cloud-ide --push;
+# docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/go-code-server:go1.18-code4.3.0-v1 -f ./dockerfiles/cloud-ide/go.Dockerfile ./dockerfiles/cloud-ide --push;
 # docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/java-code-server:java8-11-18-code4.3.0-v1 -f ./dockerfiles/cloud-ide/java.Dockerfile ./dockerfiles/cloud-ide --push;
+
+
+# build dbg-tool
+docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/dbg-tool:v1 -f ./dockerfiles/dbg-tool/Dockerfile ./dockerfiles/dbg-tool --push;
