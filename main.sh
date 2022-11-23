f82c607d726f567ce63008aab4a2d10acd2e939a
linkinghack <linkinghack@outlook.com>
2022-11-23 23:21:35 +0800

    Build nic-tool image

--- main.sh
+++ main.sh
@@ -23,4 +23,7 @@ docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
 
 # build VM base image for Virtink VM for ClusterAPI clusters
 # docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/vm-rootfs:k125-3 -f ./dockerfiles/vm/vm-rootfs-k1.25/Dockerfile ./dockerfiles/vm/vm-rootfs-k1.25 --push;
-docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/vm-rootfs:k124-0 -f ./dockerfiles/vm/vm-rootfs-k1.24.0/Dockerfile ./dockerfiles/vm/vm-rootfs-k1.24.0 --push;
+# docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/vm-rootfs:k124-0 -f ./dockerfiles/vm/vm-rootfs-k1.24.0/Dockerfile ./dockerfiles/vm/vm-rootfs-k1.24.0 --push;
+
+# build NicTool image
+docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/nictool:221123 -f ./dockerfiles/nictool/Dockerfile ./dockerfiles/nictool --push;
