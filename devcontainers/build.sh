f81c8919547b5e0042784bb417bf474ff405b109
linkinghack <linkinghack@outlook.com>
2023-08-18 14:47:54 +0800

    retry: build devcontainer

--- devcontainers/build.sh
+++ devcontainers/build.sh
@@ -10,7 +10,7 @@ chmod a+x ./init_nodejs.sh
 export IMAEG_NAME="linkinghack/devcontainer-base:bullseye-2308-1"
 export BUILD_DIR="./base-image/debian"
 
-devcontainer build \
+/usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
     --workspace-folder ${BUILD_DIR} \
     --config ${BUILD_DIR}/devcontainer.json \
     --image-name ${IMAGE_NAME} \
