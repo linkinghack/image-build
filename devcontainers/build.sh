1a27d7f21fe65eb4a6beda861bd945604e490ab6
linkinghack <linkinghack@outlook.com>
2023-08-22 16:03:50 +0800

    fix: wrong variable name ref in build script

--- devcontainers/build.sh
+++ devcontainers/build.sh
@@ -7,12 +7,12 @@ chmod a+x ./init_nodejs.sh
 ./init_nodejs.sh
 . ~/.profile
 
-export IMAEG_NAME="linkinghack/devcontainer-base:bullseye-2308-1"
-export BUILD_DIR="./base-image/debian"
+IMAGE_NAME="linkinghack/devcontainer-base:bullseye-2308-1"
+BUILD_DIR="./base-image/debian"
 
 /usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
-    --workspace-folder ${BUILD_DIR} \
-    --config ${BUILD_DIR}/devcontainer.json \
-    --image-name ${IMAGE_NAME} \
     --platform linux/arm64,linux/amd64 \
+    --image-name $IMAGE_NAME \
+    --workspace-folder $BUILD_DIR \
+    --config $BUILD_DIR/devcontainer.json \
     --push
