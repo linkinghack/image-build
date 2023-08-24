d6d62576eb74c1473ffe7e8afbe0091b5fc6ff75
linkinghack <linkinghack@outlook.com>
2023-08-24 16:58:26 +0800

    dev env: update base image

--- devcontainers/build.sh
+++ devcontainers/build.sh
@@ -7,7 +7,7 @@ chmod a+x ./init_nodejs.sh
 ./init_nodejs.sh
 . ~/.profile
 
-IMAGE_NAME="linkinghack/devcontainer-base:bullseye-2308-1"
+IMAGE_NAME="linkinghack/devcontainer-base:bullseye-2308-2"
 BUILD_DIR="./base-image/debian"
 
 /usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
