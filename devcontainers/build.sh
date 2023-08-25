4f99b7ab30e16c4b3e1622d2c1cf6fbfae95b735
linkinghack <linkinghack@outlook.com>
2023-08-25 10:24:40 +0800

    build dev env for Java

--- devcontainers/build.sh
+++ devcontainers/build.sh
@@ -7,8 +7,8 @@ chmod a+x ./init_nodejs.sh
 ./init_nodejs.sh
 . ~/.profile
 
-IMAGE_NAME="linkinghack/devcontainer-base:bullseye-2308-2"
-BUILD_DIR="./base-image/debian"
+IMAGE_NAME="linkinghack/devenv-java:bullseye-2308-1"
+BUILD_DIR="./java/"
 
 /usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
     --platform linux/arm64,linux/amd64 \
