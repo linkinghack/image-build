decfff6f94eb871d8ebb6ee7a76356cd222a358f
linkinghack <linkinghack@outlook.com>
2023-08-25 12:45:50 +0800

    fix: nodejs dev env

--- devcontainers/build.sh
+++ devcontainers/build.sh
@@ -11,24 +11,12 @@ chmod a+x ./init_nodejs.sh
 IMAGE_NAME="linkinghack/devenv-golang:bullseye-2308-1"
 BUILD_DIR="./golang"
 
-/usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
-    --platform linux/arm64,linux/amd64 \
-    --image-name $IMAGE_NAME \
-    --workspace-folder $BUILD_DIR \
-    --config $BUILD_DIR/devcontainer.json \
-    --push
-
-
-## Java
-IMAGE_NAME="linkinghack/devenv-java:bullseye-2308-1"
-BUILD_DIR="./java"
-
-/usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
-    --platform linux/arm64,linux/amd64 \
-    --image-name $IMAGE_NAME \
-    --workspace-folder $BUILD_DIR \
-    --config $BUILD_DIR/devcontainer.json \
-    --push
+# /usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
+#     --platform linux/arm64,linux/amd64 \
+#     --image-name $IMAGE_NAME \
+#     --workspace-folder $BUILD_DIR \
+#     --config $BUILD_DIR/devcontainer.json \
+#     --push
 
 
 ## NodeJS
--- devcontainers/nodejs/build-image.sh
+++ devcontainers/nodejs/build-image.sh
@@ -5,4 +5,4 @@ set -x
 docker buildx build \
     -t linkinghack/devenv-nodejs:bullseye-2308-1 \
     --platform linux/arm64,linux/amd64 \
-    --push .
+    . --push
