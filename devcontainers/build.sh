9bdf2fad75ff50c96fcf2cd12da3bdf61b11292d
Liu Lei <linkinghack@outlook.com>
2023-08-29 06:07:35 +0000

    python base amd64 only

--- devcontainers/build.sh
+++ devcontainers/build.sh
@@ -12,12 +12,15 @@ IMAGE_NAME="linkinghack/devenv-python-base:bullseye-2308-1"
 BUILD_DIR="./python"
 
 /usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
-    --platform linux/arm64,linux/amd64 \
+    --platform linux/amd64 \
     --image-name $IMAGE_NAME \
     --workspace-folder $BUILD_DIR \
     --config $BUILD_DIR/devcontainer.json \
     --push
 
+# # Python with CUDA (amd64 only)
+# docker build -t $IMAGE_NAME -f $BULID_DIR/Dockerfile $BUILD_DIR
+# docker push $IMAGE_NAME
 
 ## NodeJS
 # cd nodejs; bash build-image.sh
