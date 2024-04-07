e4e59113279489b155e96083ba6cf53b29ad56e3
LiuLei <liuleiit01@chinamobile.com>
2024-04-07 15:06:02 +0800

    update: rebuild dev env base image

--- devcontainers/build.sh
+++ devcontainers/build.sh
@@ -7,16 +7,31 @@ chmod a+x ./init_nodejs.sh
 ./init_nodejs.sh
 . ~/.profile
 
-## General
-IMAGE_NAME="linkinghack/devenv-python-base:bullseye-2308-1"
-BUILD_DIR="./dotnet"
+## General 
 
-# /usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
-#     --platform linux/arm64,linux/amd64 \
-#     --image-name $IMAGE_NAME \
-#     --workspace-folder $BUILD_DIR \
-#     --config $BUILD_DIR/devcontainer.json \
-#     --push
+## 1. Build IDE base image (including node )
+IMAGE_NAME="linkinghack/devenv-base-withnode:bullseye-2404-1"
+BUILD_DIR="./debian"
+
+# ## 2. Build Java developing environment
+# IMAGE_NAME="linkinghack/devenv-java:bullseye-2308-1"
+# BUILD_DIR="./java"
+
+# ## 3. Build Golang developing environment
+# IMAGE_NAME="linkinghack/devenv-golang:bullseye-2308-1"
+# BUILD_DIR="./golang"
+
+# ## 4. Build dotnet dev env
+# IMAGE_NAME="linkinghack/devenv-python-base:bullseye-2308-1"
+# BUILD_DIR="./dotnet"
+
+
+/usr/local/lib/nodejs/node-v18.17.1-linux-x64/bin/devcontainer build \
+    --platform linux/arm64,linux/amd64 \
+    --image-name $IMAGE_NAME \
+    --workspace-folder $BUILD_DIR \
+    --config $BUILD_DIR/devcontainer.json \
+    --push
 
 # # Python with CUDA (amd64 only)
 # docker build -t $IMAGE_NAME -f $BULID_DIR/Dockerfile $BUILD_DIR
