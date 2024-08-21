f78801cd70e8ba128b07fc4f7a696fb7baf70fb8
linkinghack <linking@linkinghack.com>
2024-08-21 15:02:56 +0800

    build IDEA java dev env image

--- main.sh
+++ main.sh
@@ -7,9 +7,12 @@ docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
 # docker build -t linkinghack/eos-cdt-1.8.1-base:ubuntu18.04 -f ./dockerfiles/eos-cdt.181.base.Dockerfile ./context;
 # docker push linkinghack/eos-cdt-1.8.1-base:ubuntu18.04;
 
-# build cloud ide images
+# build cloud ide images 
 # docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/go-code-server:go1.18-code4.4.0-v1.3 -f ./dockerfiles/cloud-ide/go.Dockerfile ./dockerfiles/cloud-ide --push;
 # docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/java-code-server:java8-11-18-code4.4.0-v1.3 -f ./dockerfiles/cloud-ide/java.Dockerfile ./dockerfiles/cloud-ide --push;
+
+## Build Jetbrains IDE image - Java IDEA
+docker buildx build --platform linux/arm64,linux/amd64 --build-arg "downloadUrl=https://download.jetbrains.com/idea/ideaIC-2021.3.2.tar.gz" -t linkinghack/java-idea21:240821-1
 
 # build VSO base image
 # docker buildx build --platform linux/arm64,linux/amd64 -t linkinghack/dev-base:bullseye-230116 -f ./dockerfiles/dev-base/Dockerfile ./dockerfiles/dev-base --push;
